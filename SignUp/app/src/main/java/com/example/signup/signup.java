8d1479d67eb1b1a09052c18b0cf8b065b4dc30a7
Prabin Sapkota <chitwan123prabin@gmail.com>
2022-02-10 09:44:09 +0545

    Upload files

---
+++ SignUp/app/src/main/java/com/example/signup/signup.java
@@ -0,0 +1,10 @@
+package com.example.signup;
+import android.app.Activity;
+import android.os.Bundle;
+public class signup extends Activity{
+@Override
+    public void onCreate(Bundle savedInstanceState){
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.signup);
+}
+}
---
+++ TableFormat/app/src/main/java/com/example/tableformat/tableview.java
@@ -0,0 +1,10 @@
+package com.example.tableformat;
+import android.app.Activity;
+import android.os.Bundle;
+public class tableview extends Activity{
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.table);
+    }
+}
