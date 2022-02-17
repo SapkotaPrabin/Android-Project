7e0b7f057d1d522ac1b88af8b5d143687f61fbcf
Prabin Sapkota <chitwan123prabin@gmail.com>
2022-02-17 20:56:17 +0545

    file add

---
+++ SignUp/app/src/main/java/com/example/signup/FirstActivity.java
@@ -0,0 +1,23 @@
+package com.example.signup;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class FirstActivity extends Activity{
+    Button next1;
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.firstactivity);
+        next1 = (Button)findViewById(R.id.next1);
+        next1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent1=new Intent(FirstActivity.this,SecondActivity.class);
+                startActivity(intent1);
+            }
+        });
+    }
+}
